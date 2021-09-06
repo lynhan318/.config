ff9c3a350a24fafb8ba10d30ec19f851e0c74cd2
Ly Nhan <lythanhnhan27294@gmail.com>
2021-09-06 10:44:19 +0700

    update config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -142,4 +142,21 @@ return require('packer').startup(function()
     --         'lukas-reineke/indent-blankline.nvim',
     --         branch='lua'
     --     }
+    -- transparent
+    use {
+        'xiyaowong/nvim-transparent',
+        config = require("transparent").setup({
+            enable = true, -- boolean: enable transparent
+            extra_groups = { -- table/string: additional groups that should be clear
+                -- In particular, when you set it to 'all', that means all avaliable groups
+
+                -- example of akinsho/nvim-bufferline.lua
+                "BufferLineTabClose", "BufferlineBufferSelected",
+                "BufferLineFill", "BufferLineBackground", "BufferLineSeparator",
+                "BufferLineIndicatorSelected"
+            },
+            exclude = {} -- table: groups you don't want to clear
+        })
+
+    }
 end)
